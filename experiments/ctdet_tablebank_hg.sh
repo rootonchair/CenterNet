5d73dca2759dca3383cf469b20bfc1453051299e
Pham Hong Vinh <phamvinh257@gmail.com>
2020-12-14 08:21:22 +0000

    allow data_dir to be modified through args

--- experiments/ctdet_tablebank_hg.sh
+++ experiments/ctdet_tablebank_hg.sh
@@ -2,15 +2,12 @@ cd src
 # train
 
 # first epoch
-#python main.py ctdet --dataset tablebank --exp_id tablebank_hg --arch hourglass --batch_size 10 --master_batch 4 --lr 2.5e-4 --load_model ../models/ctdet_coco_hg.pth --gpus 1 --num_epochs 1 --lr_step 5,10,15
+python main.py ctdet --dataset tablebank --data_dir /mnt/ai_filestore/home/vincent/TableBank/Detection --exp_id tablebank_hg --arch hourglass --batch_size 10 --master_batch 4 --lr 2.5e-4 --load_model ../models/ctdet_coco_hg.pth --gpus 1 --num_epochs 1 --lr_step 5,10,15
 
 # second epoch
-#python main.py ctdet --dataset tablebank --exp_id tablebank_hg2 --arch hourglass --batch_size 10 --master_batch 4 --lr 2.5e-8 --load_model ../models/model_last.pth --gpus 1 --num_epochs 1 --lr_step 5,10,15
+#python main.py ctdet --dataset tablebank --data_dir /mnt/ai_filestore/home/vincent/Detection --exp_id tablebank_hg2 --arch hourglass --batch_size 10 --master_batch 4 --lr 2.5e-8 --load_model ../models/model_last.pth --gpus 1 --num_epochs 1 --lr_step 5,10,15
 
 # test
-python test.py ctdet --dataset tablebank --exp_id tablebank_test --arch hourglass --load_model ../models/model_last.pth --trainval #--resume #--keep_res 
-# flip test
-#python test.py ctdet --exp_id coco_hg --arch hourglass --keep_res --resume --flip_test 
-# multi scale test
-#python test.py ctdet --exp_id coco_hg --arch hourglass --keep_res --resume --flip_test --test_scales 0.5,0.75,1,1.25,1.5
+#python test.py ctdet --dataset tablebank --data_dir /mnt/ai_filestore/home/vincent/TableBank/Detection --exp_id tablebank_test --arch hourglass --load_model ../exp/ctdet/tablebank_hg/model_last.pth --trainval #--resume #--keep_res 
+
 cd ..
--- src/lib/opts.py
+++ src/lib/opts.py
@@ -14,6 +14,8 @@ class opts(object):
                              help='ctdet | ddd | multi_pose | exdet')
     self.parser.add_argument('--dataset', default='coco',
                              help='coco | kitti | coco_hp | pascal')
+    self.parser.add_argument('--data_dir', default='./data',
+                             help='data root dir')
     self.parser.add_argument('--exp_id', default='default')
     self.parser.add_argument('--test', action='store_true')
     self.parser.add_argument('--debug', type=int, default=0,
@@ -269,7 +271,7 @@ class opts(object):
     print('training chunk_sizes:', opt.chunk_sizes)
 
     opt.root_dir = os.path.join(os.path.dirname(__file__), '..', '..')
-    opt.data_dir = '/mnt/ai_filestore/home/vincent/TableBank/Detection'#os.path.join(opt.root_dir, 'data')
+    #opt.data_dir = '/mnt/ai_filestore/home/vincent/TableBank/Detection'#os.path.join(opt.root_dir, 'data')
     opt.exp_dir = os.path.join(opt.root_dir, 'exp', opt.task)
     opt.save_dir = os.path.join(opt.exp_dir, opt.exp_id)
     opt.debug_dir = os.path.join(opt.save_dir, 'debug')
